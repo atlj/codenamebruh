aa682fcbd1e2d95174aee8b3d05b1202ea236e7e
atlj <cccswagboyyolo@yahoo.com>
2021-01-23 18:12:56 +0300

    feat: implement pervert module
    Implement a module that plays sound when a user connects to a channel or changes their channel

--- src/discord/client.js
+++ src/discord/client.js
@@ -1,8 +1,23 @@
 import Discord from 'discord.js';
 import MessageHandler from '../modules/MessageHandler.js';
+import {Users} from '../modules/Pervert.js';
+import fs from 'fs';
 
 const Client = new Discord.Client();
 
+//Events
 Client.on('message', (message) => MessageHandler(message));
-
+Client.on(
+  'voiceStateUpdate',
+  (oldState, newState) => {
+    //if (oldState.id in Users) {
+    console.log(newState);
+    if (oldState.channel !== newState.channel) {
+      newState.channel.join().then((connection) => {
+        //connection.play('../../config/pervert/default.mp3');
+      });
+    }
+  },
+  //}
+);
 export default Client;
---
+++ src/modules/Pervert.js
@@ -0,0 +1,3 @@
+//import Users from '../../config/pervert/Users.json';
+const Users = [];
+export {Users};
