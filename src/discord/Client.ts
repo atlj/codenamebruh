8e2548b7e89b2828ed6b240d8f8f475f05cdf270
atlj <cccswagboyyolo@yahoo.com>
2021-01-24 13:57:50 +0300

    feat: added pervert

--- src/discord/Client.ts
+++ src/discord/Client.ts
@@ -1,9 +1,11 @@
 import Discord from "discord.js"
 import MessageHandler from "../modules/MessageHandler"
+import Pervert from "../modules/Pervert"
 
 const Client = new Discord.Client()
 
 // Handlers
 Client.on("message", message=>MessageHandler(message))
+Client.on("voiceStateUpdate", (oldState, newState)=>Pervert(oldState, newState))
 
 export default Client
---
+++ src/modules/Pervert.ts
@@ -0,0 +1,7 @@
+import Discord from "discord.js"
+
+
+export default (oldState:Discord.VoiceState, newState:Discord.VoiceState):void=>{
+
+
+}
