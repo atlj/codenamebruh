39d597cf9dd9de9127d096ee4f1e8f17eae710a5
atlj <cccswagboyyolo@yahoo.com>
2021-02-19 16:03:42 +0300

    feat: add Help.ts

---
+++ src/modules/Help.ts
@@ -0,0 +1,5 @@
+//TODO
+//implement printing required arguments using parser.py
+//every command will have its own help class.
+//we can maybe integrate help for every argument.
+//integrate example commands
